797fcb08645401d7b3080ae7ce6e6e9d65243e0f
falcatamaster <81315959+falcatamaster@users.noreply.github.com>
2021-11-09 23:30:35 +0900

    Update js

--- js/main.js
+++ js/main.js
@@ -7,32 +7,40 @@
         loc = 0 ;
     }
     
-    const words = [
-        'wanderwall',
-        'whatever',
-        'live forever',
-        'morning glory'
-    ];
+    let words;
     let word;
     let loc;
     let startTime;
     let isPlaying = false;
     const target = document.getElementById('target');
+    const result = document.getElementById('result');
 
     document.addEventListener('click',() => {
         if (isPlaying) {
             return;
         }
 
+        words = [
+            'wanderwall',
+            'whatever',
+            'live forever',
+            'morning glory',
+            'some might say',
+            'the importance of being idle',
+            'acquiesce',
+            'supersonic',
+            'songbird'
+        ];
+        result.textContent = ``;
         isPlaying = true;
         startTime = Date.now();
         setWord();
     })
 
     document.addEventListener('keydown', e => {
-        if (e.key !== word[loc]) {
-            return;
-        }
+        // if (e.key !== word[loc]) {
+        //     return;
+        // }
 
         loc++;
 
@@ -42,11 +50,10 @@
         target.textContent = '_'.repeat(loc) + word.substring(loc);
 
         if (loc === word.length) {
-            if (words.length === 0) {
+            if (words.length === 3) {
                 const elapsedTime = ((Date.now() - startTime) / 1000).toFixed(2);
-                const result = document.getElementById('result');
                 result.textContent = `Finished! ${elapsedTime} seconds! Click to start`;
-                //isPlaying = false;
+                isPlaying = false;
                 return;
             }
 
